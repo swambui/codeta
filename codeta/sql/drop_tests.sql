0aa3bb70c542831a3aed9b59a6bc4dcbefdea962
bmoar <pyjedi@gmail.com>
2014-02-13 10:16:14 -0500

    Add sql script to drop tables for unit tests

---
+++ codeta/sql/drop_tests.sql
@@ -0,0 +1,14 @@
+/*
+    delets all the data in teh table
+    for each unit test
+*/
+
+delete from Users;
+delete from Instructor;
+delete from InstructorTeachesCourse;
+delete from StudentEnrollsCourse;
+delete from Assignment;
+delete from UserFile;
+delete from Submission;
+delete from SubmissionUpload;
+delete from AssignmentUpload;
